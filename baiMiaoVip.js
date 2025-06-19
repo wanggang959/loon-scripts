fab1c5b157c87ad56d3afbf41010c95bbf46ed79
wanggang959 <2326488943@qq.com>
2025-06-19 11:57:16 +0800

    Update baiMiaoVip.js

--- baiMiaoVip.js
+++ baiMiaoVip.js
@@ -4,8 +4,8 @@ var obj = JSON.parse(body);
 
 
 
-obj.value.vips = [1,2];
-obj.value.vip=2;
+obj.value.vips = ["普通会员","黄金会员"];
+obj.value.vip="黄金会员";
 
 body = JSON.stringify(obj);
 $done( {body} );
